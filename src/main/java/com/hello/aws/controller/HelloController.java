495e30f5f95280540e989dc30e6a9f5bd69c7903
wenhaoliu <wliu1197@uni.sydney.edu.au>
2025-01-19 18:07:07 +1100

    push and trigger auto aws CodePipeline deploy CI/CD

--- src/main/java/com/hello/aws/controller/HelloController.java
+++ src/main/java/com/hello/aws/controller/HelloController.java
@@ -1,6 +1,5 @@
 package com.hello.aws.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +7,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/rest")
 public class HelloController {
+    //whenever we push code to github AWS CodePipeline will detect it and build the image to ECR
+    //then CodeDeploy will fetch image runs image in ECS service with 2 tasks running for this MS
+    //Application Load balancer is applied to this running tasks
     @GetMapping(path = "/hello")
     public String sayHello(){
-        return "Hello world AWS CodePipeline CI/CD auto deploy";
+        return "Hello world AWS CodePipeline CI/CD auto deploy let's do it";
     }
 }
